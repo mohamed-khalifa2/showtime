753664b1d5cf0a07cda7693779db434026542f04
mohamed-khalifa2 <runeaaberg1@gmail.com>
2025-03-17 12:58:38 +0200

    spinner to details page

--- src/app/pages/movie-details/movie-details.component.ts
+++ src/app/pages/movie-details/movie-details.component.ts
@@ -4,12 +4,12 @@ import { ActivatedRoute } from '@angular/router';
 import { NgStyle } from '@angular/common';
 import { SafePipe } from '../../pipes/safe.pipe';
 import { forkJoin } from 'rxjs';
-
+import { SpinnerComponent } from '../../shared/spinner/spinner.component';
 
 @Component({
   selector: 'app-movie-details',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [NgStyle, SafePipe],
+  imports: [NgStyle, SafePipe, SpinnerComponent],
   templateUrl: './movie-details.component.html',
   styleUrl: './movie-details.component.css'
 })
@@ -21,6 +21,7 @@ export class MovieDetailsComponent {
   photo = ''
   video: any = {}
   paramId: any = ''
+  loading: boolean = true
 
   ngOnInit() {
     this.paramId = this.router.snapshot.paramMap.get('id');
@@ -35,6 +36,7 @@ export class MovieDetailsComponent {
         this.photo = `https://image.tmdb.org/t/p/w1920_and_h800_multi_faces${details.backdrop_path}`;
         this.video = videos.results.find((video) => video.type === "Trailer");
         this.movieCredits = credits;
+        this.loading = false;
 
       },
       error: (err) => console.error('Error fetching movie data:', err),
